0ff909cd716eb66ca153e78abd40228a122b4664
aresxin <653992778@qq.com>
2022-04-26 17:40:07 +0800

    add

--- SwiftHelper/SwiftHelper/Code/Collection+Extension.swift
+++ SwiftHelper/SwiftHelper/Code/Collection+Extension.swift
@@ -5,18 +5,29 @@
 
 import Foundation
 
-// Array Sort by keyPath
-extension Collection{
+public extension Collection{
+    // MARK: Array Sort by keyPath
     func sorted<Value: Comparable>(
         by keyPath: KeyPath<Element, Value>,
-        _ comparator: (_ lhs: Value, _ rhs: Value)->Bool) -> [Element]{
+        _ comparator: (_ lhs: Value, _ rhs: Value)->Bool) -> [Element] {
             return self.sorted{
                 comparator($0[keyPath: keyPath], $1[keyPath: keyPath])
             }
         }
+    
+    // MARK: Diff by keyPath
+    func diff<Value: Comparable, D>(from diff: [D], compareKey compareKeyPath: KeyPath<D, Value>,
+                                    ownKey keyPath: KeyPath<Element, Value>) -> [Self.Element] {
+        guard !diff.isEmpty else { return [] }
+        
+        let difference = self.filter({ currentObject in
+            (diff.contains(where: { $0[keyPath: compareKeyPath] == currentObject[keyPath: keyPath] }))
+        })
+        return difference
+    }
 }
 
-extension Sequence {
+public extension Sequence {
     func toDictionary<Key: Hashable, Value>(where closure: (Element) -> (Key, Value)) -> [Key: Value] {
         reduce(into: [Key: Value]()) { (result, element) in
             let components = closure(element)
@@ -28,6 +39,43 @@ extension Sequence {
         reduce(into: [Key: Value]()) { (result, element) in
             guard let components = closure(element) else { return }
             result[components.0] = components.1
+        }
+    }
+}
+
+
+
+public extension Dictionary {
+    init<S>(_ values: S, uniquelyKeyedBy keyPath: KeyPath<S.Element, Key>) where S : Sequence, S.Element == Value {
+        let keys = values.map { $0[keyPath: keyPath] }
+
+        self.init(uniqueKeysWithValues: zip(keys, values))
+    }
+}
+
+
+// MARK: Unordered example
+public extension Sequence {
+    func merge<T: Sequence, U: Hashable>(mergeWith: T, uniquelyKeyedBy: KeyPath<T.Element, U>) -> [Element] where T.Element == Element {
+        let dictOld = Dictionary(self, uniquelyKeyedBy: uniquelyKeyedBy)
+        let dictNew = Dictionary(mergeWith, uniquelyKeyedBy: uniquelyKeyedBy)
+
+        return dictNew.merging(dictOld, uniquingKeysWith: { old, new in old }).map { $0.value }
+    }
+}
+
+// MARK: Ordered example
+public extension Array {
+    mutating func mergeWithOrdering<U: Hashable>(mergeWith: Array, uniquelyKeyedBy: KeyPath<Array.Element, U>) {
+        let dictNew = Dictionary(mergeWith, uniquelyKeyedBy: uniquelyKeyedBy)
+
+        for (key, value) in dictNew {
+            guard let index = firstIndex(where: { $0[keyPath: uniquelyKeyedBy] == key }) else {
+                append(value)
+                continue
+            }
+
+            self[index] = value
         }
     }
 }
--- SwiftHelper/SwiftHelper/Test.playground/Contents.swift
+++ SwiftHelper/SwiftHelper/Test.playground/Contents.swift
@@ -28,3 +28,20 @@ var url = URL(string: "https://www.example.com")!
 url.appendQueryItem(name: "name", value: "bhuvan")
 
 
+// Extension Array Merge
+struct SomeStruct: Hashable {
+    let id: Int
+    let name: String
+}
+
+let someStruct1 = SomeStruct(id: 1, name: "1")
+let someStruct2 = SomeStruct(id: 2, name: "2")
+let someStruct3 = SomeStruct(id: 2, name: "3")
+let someStruct4 = SomeStruct(id: 4, name: "4")
+
+var arrayA = [someStruct1, someStruct2]
+let arrayB = [someStruct3, someStruct4]
+
+let arrayMerge = arrayA.merge(mergeWith: arrayB, uniquelyKeyedBy: \.id)
+
+arrayA.mergeWithOrdering(mergeWith: arrayB, uniquelyKeyedBy: \.id)
