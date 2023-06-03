bc740bbbe660aedf8a0432c6dff258086cb50a0f
aresxin <yangjinxin824@icloud.com>
2023-06-03 21:27:36 +0800

    add demo

--- SwiftHelper/SwiftHelper/AttributedStringTest.playground/Contents.swift
+++ SwiftHelper/SwiftHelper/AttributedStringTest.playground/Contents.swift
@@ -9,5 +9,15 @@ let build = NSAttributedString {
 }
 
 
-
+let build1 = NSMutableAttributedString()
+let start = NSAttributedString(string: "Start", attributes: [
+    NSAttributedString.Key.foregroundColor: UIColor.red
+])
+build1.append(start)
+build1.append(NSAttributedString(string: " "))
+let end = NSAttributedString(string: "End", attributes: [
+    NSAttributedString.Key.foregroundColor: UIColor.yellow,
+    NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18)
+])
+build1.append(end)
 
