9be4ac6dbde5b307144f0d0435b4d37fb03fabab
aresxin <yangjinxin824@icloud.com>
2023-06-04 10:29:27 +0800

    Update Contents.swift
    fix string

--- SwiftHelper/SwiftHelper/AttributedStringTest.playground/Contents.swift
+++ SwiftHelper/SwiftHelper/AttributedStringTest.playground/Contents.swift
@@ -3,19 +3,19 @@ import UIKit
 var greeting = "Hello, playground"
 
 let build = NSAttributedString {
-    Text("Start").color(.red)
+    Text("Hello").color(.red)
     SpecialCharacters.space
-    Text("End").color(.yellow).font(.boldSystemFont(ofSize: 18))
+    Text("World").color(.yellow).font(.boldSystemFont(ofSize: 18))
 }
 
 
 let build1 = NSMutableAttributedString()
-let start = NSAttributedString(string: "Start", attributes: [
+let start = NSAttributedString(string: "Hello", attributes: [
     NSAttributedString.Key.foregroundColor: UIColor.red
 ])
 build1.append(start)
 build1.append(NSAttributedString(string: " "))
-let end = NSAttributedString(string: "End", attributes: [
+let end = NSAttributedString(string: "World", attributes: [
     NSAttributedString.Key.foregroundColor: UIColor.yellow,
     NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18)
 ])
