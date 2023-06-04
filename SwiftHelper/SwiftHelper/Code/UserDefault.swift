d46dad03c67de7b5596131d1ffeecf029de3f084
aresxin <yangjinxin824@icloud.com>
2023-06-04 09:16:19 +0800

    add p send

--- SwiftHelper/SwiftHelper/Code/UserDefault.swift
+++ SwiftHelper/SwiftHelper/Code/UserDefault.swift
@@ -45,6 +45,8 @@ public struct UserDefault<Value> {
             } else {
                 container.set(newValue, forKey: key)
             }
+            
+            publisher.send(newValue)
         }
     }
 
